94dd565282c02398af81266e3412e4582aa0848f
gascione <gaglausiuss@gmail.com>
2024-08-05 22:41:47 -0300

    Created RouteError

--- src/components/RouteError/RouteError.jsx
+++ src/components/RouteError/RouteError.jsx
@@ -3,7 +3,15 @@ import { useRouteError } from "react-router-dom";
 
 const RouteError = () => {
   const error = useRouteError();
-  return <div>RouteError</div>;
+  return (
+    <>
+      <div>RouteError</div>
+
+      <p>
+        <i>{error.statusText || error.message}</i>
+      </p>
+    </>
+  );
 };
 
 export default RouteError;
